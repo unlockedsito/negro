1034b4bc6ea0af010fe229da273e5f9710662f63
unlockedsito <103005908+unlockedsito@users.noreply.github.com>
2022-04-04 18:35:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php
-
-include 'Telegram.php';
-
-$bot_id = '5127658543:AAGXX_9uouyc64fuv3qVVdFLESAL_TyOoSY';
-$telegram = new Telegram($bot_id);
-
-$text = $telegram->Text();
-if($text == '/start') {
-	$telegram->sendMessage(array(
-		'chat_id' => $telegram->ChatID(),
-		'text' => 'Hi, how may i help u?'
-	));
-}
+<?php 
+$bot_token = 'YOUR-BOT-TOKEN';
+$website = 'https://api.telegram.org/bot'.$bot_token;
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+if($message == '/start'){
+	sendMessage($chatId, 'Hi, how may i help u?');
+} else if($message == 'hi') {
+	sendMessage($chatId, 'Hi, fucking bitch!');
+} else if($message == 'ola') {
+	sendMessage($chatId, 'se dice "Hola" aprende a escribir bien CTM o te vas de mi iglesia :V');
+} else if($message == 'hola') {
+	sendMessage($chatId, 'Tu nariz contra mi bola :V');
